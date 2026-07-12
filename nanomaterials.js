af3197b91ead2c045ddc10ca0ae7699e34108171
Sami A.M Abdullah <samiabdullah2500@gmail.com>
2026-07-12 13:05:06 +0530

    Remove all repository files
    Delete all project files including source code, documentation, configuration, tests, and assets. This clears the repository contents entirely.

--- nanomaterials.js
+++
@@ -1,5 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64">
-  <rect width="64" height="64" rx="14" fill="#0e7c86"/>
-  <polyline points="8,42 17,42 22,18 28,50 34,26 40,42 46,34 56,34"
-    fill="none" stroke="#ffffff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
